d1f0f01aa4957aa84ac318fe5e2356f1331c3953
Fahim Alam <66892906+Fahim-Alam@users.noreply.github.com>
2021-12-13 02:34:45 -0500

    Add files via upload

---
+++ src/components/Tracker.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Tracker = ({
+  name,
+  image,
+  symbol,
+  price,
+  volume,
+  priceChange,
+  marketCap,
+}) => {
+  return (
+    <div className="tracker-container">
+      <div className="coin-row">
+        <div className="coin-info">
+          <img src={image} alt="logo" />
+          <h1>{name}</h1>
+          <p className="symbol">{symbol.toUpperCase()}</p>
+        </div>
+        <div className="data">
+          <p className="price">
+            ${parseFloat(price.toFixed(2)).toLocaleString()}
+          </p>
+          <p className="volume">
+            ${parseFloat(volume.toFixed(2)).toLocaleString()}
+          </p>
+          {priceChange > 0 ? (
+            <p className="percent up">{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className="percent down">{priceChange.toFixed(2)}%</p>
+          )}
+          <p className="market-cap">Cap: ${marketCap.toLocaleString()}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tracker;
